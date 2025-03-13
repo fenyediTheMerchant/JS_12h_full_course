28e22e781d91c97821848da068dcbde3d284c384
fenyediTheMerchant <fenyedi.levente@gmail.com>
2025-03-13 23:29:21 +0200

    11. Checked Property

--- index.js
+++ index.js
@@ -1,17 +1,37 @@
-const myText = document.getElementById("myText");
+// .checked = property that determines the chacked state
+// of a HTML checkbox or radio button element
+
+// Storing elements in constants to work easier
+const myCheckbox = document.getElementById("myCheckbox");
+const visaBtn = document.getElementById("visaBtn");
+const masterCardBtn = document.getElementById("masterCardBtn");
+const payPalBtn = document.getElementById("payPalBtn");
 const mySubmit = document.getElementById("mySubmit");
-const resultElement = document.getElementById("resultElement");
-let age = 0;
+const subResult = document.getElementById("subResult");
+const paymentResult = document.getElementById("paymentResult");
 
 mySubmit.onclick = function(){
-    age = Number(myText.value);
-    console.log(age);
-    if(age >= 50){
-        console.log("You are over 50.");
-        resultElement.textContent = `Your age is ${age}`;
+    if(myCheckbox.checked){
+        subResult.textContent = `You are Subscribed!`;
     }
     else{
-        console.log("You are under 50.");
+        subResult.textContent = `You are NOT Subscribed!`;
+    }
+
+    if(visaBtn.checked){
+        paymentResult.textContent = `You are paying with Visa`;
+    }
+    else if(masterCardBtn.checked){
+        paymentResult.textContent = `You are paying with MasterCard`;
+
+    }
+    else if(payPalBtn.checked){
+        paymentResult.textContent = `You are paying with PayPal`;
+
+    }
+    else {
+        paymentResult.textContent = `You must select a payment type!`;
+
     }
 }
 
