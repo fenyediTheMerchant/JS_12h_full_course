ae1ce914fd46b8ad8a1832eefdfb099decfa7c79
fenyediTheMerchant <fenyedi.levente@gmail.com>
2025-03-13 21:41:14 +0200

    3. Arithmetics

--- index.js
+++ index.js
@@ -1,45 +1,29 @@
-//variable = A container that stores a value. Behaves like a container
-// 1. declaration let x;
-// 2. assignment x = 100;
+let students = 30;
 
-// // Numbers!
-// let x = 123;
-// // console.log(x);
+// Reassign the variable
+students = students + 1;
+console.log(students);
 
-// let age = 25;
-// let price = 10.99;
-// let gpa = 2.1;
+students = students - 1;
+console.log(students);
 
-// // With backticks
-// console.log(`You are ${age} years old`);
-// // With strings
-// console.log("You are " + age + " years old");
-// // Write out datatype
-// console.log(typeof price);
+students = students * 2;
+console.log(students);
 
-// // Strings!
-// let firstName = "Levente";
-// let favoriteFood = "pizza";
-// let email = "fenyedi.levente123@gmail.com";
+students = students / 2;
+console.log(students);
 
-// console.log(typeof firstName);
-// console.log(firstName);
-// console.log(`Your name is ${firstName}`);
-// console.log(`You like ${favoriteFood}`);
-// console.log(`You email is ${email}`);
+// To the power of 2..
+students = students ** 2;
+console.log(students);
 
-// // Booleans!
-// let online = true;
+students = students % 2;
+console.log(students);
 
-// console.log(typeof online);
-// console.log(`${firstName} is online: ${online}`);
+// To not brake students...
+let extraStudents = students % 2;
 
-// Variable Example
+// Or use shorter forms
 
-let fullName = "Fenyedi Levente";
-let age = 25;
-let isStudent = false;
-
-document.getElementById("p1").textContent = `Your name is ${fullName}`;
-document.getElementById("p2").textContent = `Your age is ${age} old`;
-document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
+students += 1;
+console.log(students);
