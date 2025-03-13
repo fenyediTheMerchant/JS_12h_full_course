f40848d00844bd34f967766ff95f70998ae012a7
fenyediTheMerchant <fenyedi.levente@gmail.com>
2025-03-13 21:03:14 +0200

    2. Variables

--- index.js
+++ index.js
@@ -1,7 +1,45 @@
-console.log(`Hello`);
-console.log(`I like pizza!`);
-window.alert(`This is an alert`);
-window.alert(`I like pizza`);
+//variable = A container that stores a value. Behaves like a container
+// 1. declaration let x;
+// 2. assignment x = 100;
 
-document.getElementById("myH1").textContent = `Hello`;
-document.getElementById("myP").textContent = `I like pizza`;
+// // Numbers!
+// let x = 123;
+// // console.log(x);
+
+// let age = 25;
+// let price = 10.99;
+// let gpa = 2.1;
+
+// // With backticks
+// console.log(`You are ${age} years old`);
+// // With strings
+// console.log("You are " + age + " years old");
+// // Write out datatype
+// console.log(typeof price);
+
+// // Strings!
+// let firstName = "Levente";
+// let favoriteFood = "pizza";
+// let email = "fenyedi.levente123@gmail.com";
+
+// console.log(typeof firstName);
+// console.log(firstName);
+// console.log(`Your name is ${firstName}`);
+// console.log(`You like ${favoriteFood}`);
+// console.log(`You email is ${email}`);
+
+// // Booleans!
+// let online = true;
+
+// console.log(typeof online);
+// console.log(`${firstName} is online: ${online}`);
+
+// Variable Example
+
+let fullName = "Fenyedi Levente";
+let age = 25;
+let isStudent = false;
+
+document.getElementById("p1").textContent = `Your name is ${fullName}`;
+document.getElementById("p2").textContent = `Your age is ${age} old`;
+document.getElementById("p3").textContent = `Enrolled: ${isStudent}`;
