506825f3dc42dd7c1ce322f55c4dec3c642be557
fenyediTheMerchant <fenyedi.levente@gmail.com>
2025-03-14 16:28:38 +0200

    25. Arrays

--- index.js
+++ index.js
@@ -1,24 +1,37 @@
-// Temp conv.
+// array = a variable like a structure that can hold more
+//  than 1 value
 
-const textBox = document.getElementById("textBox");
-const toFahrenheit = document.getElementById("toFahrenheit");
-const toCelsius = document.getElementById("toCelsius");
-const result = document.getElementById("result");
+//let fruit = "apple"; // fruit variable is of value "apple"
+let fruits = ["apple","orange", "banana"];
 
-let temp;
+// fruits[3] = "coconut";
+// fruits.push("mango");
+// fruits.pop();
+fruits.unshift("mango");
+// fruits.shift();
 
-function convert(){
-    if(toFahrenheit.checked){
-        temp = Number(textBox.value);
-        temp = temp * 9 / 5 + 32;
-        result.textContent = temp.toFixed(1) + "°F";
-    }
-    else if(toCelsius.checked){
-        temp = Number(textBox.value);
-        temp = (temp - 32) * (5/9);
-        result.textContent = temp + "°C";
-    }
-    else{
-        result.textContent = "Select a Unit!";
-    }
+console.log(fruits[0]);
+console.log(fruits[1]);
+console.log(fruits[2]);
+console.log(fruits[3]);
+console.log(fruits);
+
+let numOfFruits = fruits.length;
+let index = fruits.indexOf("apple");
+
+console.log(numOfFruits)
+console.log(index)
+
+// for(let i = 0; i < fruits.length; i++){
+//     console.log(fruits[i]);
+// }
+
+// for(let i = fruits.length - 1; i >= 0; i--){
+//     console.log(fruits[i]);
+// }
+
+fruits.sort().reverse();
+
+for(let fruit of fruits){
+    console.log(fruit);
 }
