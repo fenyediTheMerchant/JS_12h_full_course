1aa067234f5f4abfbaf1b2fb134fcc7fa77c4614
fenyediTheMerchant <fenyedi.levente@gmail.com>
2025-03-14 15:50:55 +0200

    23. Variable Scope

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
-// function =  A section of reusable code.
-// Declare code once, use it whenever you want.
-// Call the function to execute that code.
+// Variable Scope = Where a variable is recognized and
+// accessible (local vs global)
 
-// function happyBirthday(username, age){
-//     console.log(`Happy Birthday to you!`);
-//     console.log(`Happy Birthday to you!`);
-//     console.log(`Happy Birthday dear ${username}!`);
-//     console.log(`Happy Birthday to you!`);
-//     console.log(`You are ${age} years old.`);
-// }
-// // Passing arguments to the function
-// happyBirthday("Fenyedi Levente", 25);
-// happyBirthday("SpongeBob", 30);
-// happyBirthday("Patrick", 37);
+let x = 3;
 
-
-function add(x, y){
-    return x + y;
-}
-function substract(x, y){
-    return x - y;
-}
-function multiply(x, y){
-    return x * y;
-}
-// etc...
-function isEven(x){
-    return (x % 2 === 0) ? true : false;
-}
-function isValidEmail(email){
-    return (email.includes("@") && email.includes(".")) ? true : false;
+function function1(){
+    let x = 1;
+    console.log(x);
 }
 
-console.log(add(20,35));
-console.log(substract(20,35));
-console.log(multiply(20,35));
-console.log(isEven(19));
-console.log(isValidEmail("fenyedi.leventefake.com"));
+function function2(){
+    let x = 2;
+    console.log(x);
+}
+
+function1();
