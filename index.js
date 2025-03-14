4ae56b0c741df95236e11020df9658eb21f83abd
fenyediTheMerchant <fenyedi.levente@gmail.com>
2025-03-14 15:36:46 +0200

    22. Functions!

--- index.js
+++ index.js
@@ -1,39 +1,39 @@
-// Number guessing game!!
+// function =  A section of reusable code.
+// Declare code once, use it whenever you want.
+// Call the function to execute that code.
 
-const minNum = 50;
-const maxNum = 100;
+// function happyBirthday(username, age){
+//     console.log(`Happy Birthday to you!`);
+//     console.log(`Happy Birthday to you!`);
+//     console.log(`Happy Birthday dear ${username}!`);
+//     console.log(`Happy Birthday to you!`);
+//     console.log(`You are ${age} years old.`);
+// }
+// // Passing arguments to the function
+// happyBirthday("Fenyedi Levente", 25);
+// happyBirthday("SpongeBob", 30);
+// happyBirthday("Patrick", 37);
 
-const answerNum = 
-Math.floor(Math.random() * 
-(maxNum - minNum + 1)) + minNum;
 
-let attempts = 0;
-let guess;
-let running = true;
-
-while(running){
-    guess = window.prompt(`Guess a number between ${minNum} and ${maxNum}:`)
-    guess = Number(guess);
-
-    if(isNaN(guess)){
-        window.alert("Please enter a valid number!");
-    }
-    else if(guess < minNum || guess > maxNum){
-        window.alert("Please enter a valid number!");
-    }
-    else{
-        attempts++;
-        if(guess < answerNum){
-            window.alert("Too Low! Try Again!");
-        }
-        else if(guess > answerNum){
-            window.alert("Too High! Try Again!");
-        }
-        else{
-            window.alert(`Correct!! The answer was ${answerNum}. It took you ${attempts} attempts.`);
-            running = false;
-        }
-    }
-
-    console.log(typeof guess , guess, answerNum);
+function add(x, y){
+    return x + y;
 }
+function substract(x, y){
+    return x - y;
+}
+function multiply(x, y){
+    return x * y;
+}
+// etc...
+function isEven(x){
+    return (x % 2 === 0) ? true : false;
+}
+function isValidEmail(email){
+    return (email.includes("@") && email.includes(".")) ? true : false;
+}
+
+console.log(add(20,35));
+console.log(substract(20,35));
+console.log(multiply(20,35));
+console.log(isEven(19));
+console.log(isValidEmail("fenyedi.leventefake.com"));
