9ef5651022eeb67e7287c2189e784c9aa39edb2e
fenyediTheMerchant <fenyedi.levente@gmail.com>
2025-03-14 15:02:44 +0200

    21. Number Guessing Game

--- index.js
+++ index.js
@@ -1,8 +1,39 @@
-// for loop = repeat some code a Limited amount of times
+// Number guessing game!!
 
-for(let i = 1; i <= 20; i++){
-    if(i == 13){
-        break;
+const minNum = 50;
+const maxNum = 100;
+
+const answerNum = 
+Math.floor(Math.random() * 
+(maxNum - minNum + 1)) + minNum;
+
+let attempts = 0;
+let guess;
+let running = true;
+
+while(running){
+    guess = window.prompt(`Guess a number between ${minNum} and ${maxNum}:`)
+    guess = Number(guess);
+
+    if(isNaN(guess)){
+        window.alert("Please enter a valid number!");
     }
-    console.log(i);
+    else if(guess < minNum || guess > maxNum){
+        window.alert("Please enter a valid number!");
+    }
+    else{
+        attempts++;
+        if(guess < answerNum){
+            window.alert("Too Low! Try Again!");
+        }
+        else if(guess > answerNum){
+            window.alert("Too High! Try Again!");
+        }
+        else{
+            window.alert(`Correct!! The answer was ${answerNum}. It took you ${attempts} attempts.`);
+            running = false;
+        }
+    }
+
+    console.log(typeof guess , guess, answerNum);
 }
