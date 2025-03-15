a12165f4cb92db57c7c69ffb25730b4b3d7af17a
fenyediTheMerchant <fenyedi.levente@gmail.com>
2025-03-15 19:23:42 +0200

    41. Static Keyword

--- index.js
+++ index.js
@@ -1,61 +1,63 @@
-// class = (ES6 feature) provides a more structured
-// and cleaner way to work with objects compared to
-// traditional constructor functions
-// ex.: static keyword, encapsulation, inheritance
+// static = keyword that defines properties or methods
+// that belong to a class itself rather than the objects
+// created from that class
+// (class own anything static, not the objects)
 
-// Constructor ex.
-// function Product(name, price){
-//     this.name = name;
-//     this.price = price;
-
-//     this.displayProduct = function(){
-//         console.log(`Product: ${this.name}`);
-//         console.log(`Price: $${this.price.toFixed(2)}`);
-//     };
-
-//     this.calculateTotal = function(salesTax){
-//         return this.price + (this.price * salesTax);
-//     }
-// }
-
-class Product{
-    constructor(name, price){
-        this.name = name;
-        this.price = price;
-    }
-
-    // function
-    displayProduct(){
-        console.log(`Product: ${this.name}`);
-        console.log(`Price: ${this.price.toFixed(2)} RON`);
-    }
-
-    displayTotal(salesTax){
-        return this.price * (1 + salesTax);
-    }
+class MathUtil{
+    static PI = 3.14159;
     
-    calculateTotal(salesTax){
-        return this.price + (this.price * salesTax);
+    static getDiameter(radius){
+        return radius * 2;
+    }
+
+    static getCircumference(radius){
+        return 2 * this.PI * radius;
+    }
+
+    static getArea(radius){
+        return this.PI * radius * radius;
     }
 }
 
-const salesTax = 0.19;
+const MathUtil1 = new MathUtil();
 
-const product1 = new Product("Hamburger", 38);
-// Instead console log, we invoke the function
-product1.displayProduct();
-const total = product1.calculateTotal(salesTax);
-console.log(`Total price (with tax): ${total.toFixed(2)} RON`);
-
-const product2 = new Product("Pizza", 24);
-// Instead console log, we invoke the function
-product2.displayProduct();
-const total2 = product2.calculateTotal(salesTax);
-console.log(`Total price (with tax): ${total2.toFixed(2)} RON`);
+// Works
+console.log(MathUtil.PI);
+console.log(MathUtil.getDiameter(10));
+console.log(MathUtil.getCircumference(10));
+console.log(MathUtil.getArea(10));
+// Doesnt work
+console.log(MathUtil1.PI);
 
 
-const product3 = new Product("Bread", 3);
-// Instead console log, we invoke the function
-product3.displayProduct();
-const total3 = product3.calculateTotal(salesTax);
-console.log(`Total price (with tax): ${total3.toFixed(2)} RON`);
+// Example 2
+
+class User{
+    // Store how many users are created
+    static userCount = 0;
+
+    constructor(username){
+        this.username = username;
+        User.userCount++;
+    }
+
+    static getUserCount(){
+        console.log(`There are ${User.userCount} nr. of user online.`);
+    }
+
+    sayHello(){
+        console.log(`Hello my name is ${this.username}`);
+    }
+}
+
+const user1 = new User("Spongebob");
+const user2 = new User("Patrick");
+const user3 = new User("Sandy");
+
+console.log(user1.username, User.userCount);
+console.log(user2.username, User.userCount);
+console.log(user3.username, User.userCount);
+user1.sayHello();
+user2.sayHello();
+user3.sayHello();
+User.getUserCount();
