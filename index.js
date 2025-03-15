2d3345574b01ea54fdb134a5e84ffd9beedc8c66
fenyediTheMerchant <fenyedi.levente@gmail.com>
2025-03-15 20:20:48 +0200

    44. Getters and setters

--- index.js
+++ index.js
@@ -1,75 +1,108 @@
-// super = a keyword used in classes to call the
-// constructor or acces the properites and methods of
-// a parent ("super"-class)
-// this = this object
-// supe = the parent
+// getter = special method that makes a property readable
+// setter = special method that makes a property writeable
 
-// DRY Principle here too.
-// Because every animal has a name and age, the parent
-// class should construct these.
+// validate and modify a value when reading/writing a property
+// Setter = "input validation"
+// Getter = can return additional logic
 
-class Animal{
-    constructor(name, age){
-        this.name = name;
+class Rectangle{
+    constructor(width, height){
+        this.width = width;
+        this.height = height;
+    }
+
+    set width(newWidth){
+        if(newWidth > 0){
+            this._width = newWidth 
+        }
+        else{
+            console.log("Width must be positive number.");
+        }
+    }
+    set height(newHeight){
+        if(newHeight > 0){
+            this._height = newHeight 
+        }
+        else{
+            console.log("Height must be positive number.");
+        }    
+    }
+    get width(){
+        return `${this._width.toFixed(1)} cm`;
+    }
+    get height(){
+        return `${this._height.toFixed(1)} cm`;
+    }
+
+    get area(){
+        return `${(this._width * this._height).toFixed(1)} cm ^2`;
+    }
+}
+
+// Wrong values can be solved with getters and setters
+// const rectangle = new Rectangle(-10000, "pizza");
+const rectangle = new Rectangle(3, 4);
+
+rectangle.width = -1;
+rectangle.height = -1;
+
+console.log(rectangle.width, rectangle.height);
+console.log(rectangle.area);
+
+// Ex2.
+
+class Person{
+    constructor(firstName, lastName, age){
+        this.firstName = firstName;
+        this.lastName = lastName;
         this.age = age;
     }
 
-    move(speed){
-        console.log(`The ${this.name} moves at a speed of ${speed} kmh.`);
+    set firstName(newFirstName){
+        if(typeof newFirstName === "string"
+            && newFirstName.length > 0
+        ){
+            this._firstName = newFirstName;
+        }
+        else{
+            console.error("Fist name must be a non empty string");
+        }
+    }
+    set lastName(newLastName){
+        if(typeof newLastName === "string"
+            && newLastName.length > 0
+        ){
+            this._lastName = newLastName;
+        }
+        else{
+            console.error("Last name must be a non empty string");
+        }
+    }
+    set age(newAge){
+        if(typeof newAge === "number"
+            && newAge >= 0
+        ){
+            this._age = newAge;
+        }
+        else{
+            console.error("Age must be a positive num.");
+        }
+    }
+    get firstName(){
+        return this._firstName;
+    }
+    get lastName(){
+        return this._lastName;
+    }
+    get age(){
+        return this._age;
+    }
+    get fullName(){
+        return this._firstName + " " + this._lastName
     }
 }
-class Rabbit extends Animal{
-    constructor(name, age, runSpeed){
-        super(name, age);
-        this.runSpeed = runSpeed;
-    }
 
-    run(){
-        console.log(`This ${this.name} can run!`);
-        // Extending the parent move method
-        // Use the parent move method!
-        super.move(this.runSpeed)
-    }
-}
-class Fish extends Animal{
-    constructor(name, age, swimSpeed){
-        super(name, age);
-        this.swimSpeed = swimSpeed;
-    }
+const person = new Person("Spongebob", "Squarepants", 65);
 
-    swim(){
-        console.log(`This ${this.name} can swim!`);
-        super.move(this.swimSpeed);
-    }
-}
-class Hawk extends Animal{
-    constructor(name, age, flySpeed){
-        super(name, age);
-        this.flySpeed = flySpeed;
-    }
-
-    fly(){
-        console.log(`This ${this.name} can fly!`);
-        super.move(this.flySpeed);
-    }
-}
-
-const rabbit = new Rabbit("Rabbit", 1, 25);
-const fish = new Fish("Fish", 2, 12);
-const hawk = new Hawk("Hawk", 3, 50);
-
-console.log(rabbit.name);
-console.log(rabbit.age);
-console.log(rabbit.runSpeed);
-
-console.log(fish.name);
-console.log(fish.age);
-console.log(fish.swimSpeed);
-
-console.log(hawk.name);
-console.log(hawk.age);
-console.log(hawk.flySpeed);
-
-rabbit.run();
-fish.swim();
-hawk.fly();
+console.log(person.firstName, person.lastName, person.age);
+console.log(person.fullName);
