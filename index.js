10282075ad3c14829f4d5e6638e98259550bbae5
fenyediTheMerchant <fenyedi.levente@gmail.com>
2025-03-16 20:55:15 +0200

    64. Key Events (eventlisteners)

--- index.js
+++ index.js
@@ -1,32 +1,45 @@
-// eventListener = Listen for specific events to create inter-
-// active web pages.
-// Events: click, mouseover, mouseout
-// .addEventListener(event, callback);
+// eventListener = listen for specific events to create interactive
+// web pages. Events: keydown, keyup
+// document.addEventListener(event, callback);
 
 const myBox = document.getElementById("myBox");
-const myButton = document.getElementById("myButton");
+const moveAmount = 10;
+let x = 0;
+let y = 0;
 
-function changeColor(event){
-    console.log(event);
-    myBox.style.backgroundColor = "tomato";
-    myBox.textContent = "OUCH!! 🤠"
-}
 
-myButton.addEventListener("click", event => {
-    console.log(event);
-    myBox.style.backgroundColor = "tomato";
-    myBox.textContent = "OUCH!! 🤠"
+document.addEventListener("keydown", event => {
+
+    if(event.key.startsWith("Arrow")){
+
+        event.preventDefault();
+
+        myBox.textContent = "‼️"
+        myBox.style.backgroundColor = "blue";
+        console.log(event.key);
+
+        switch(event.key){
+            case "ArrowUp":
+                y -= moveAmount;
+                break;
+            case "ArrowDown":
+                y += moveAmount;
+                break;
+            case "ArrowLeft":
+                x -= moveAmount;
+                break;
+            case "ArrowRight":
+                x += moveAmount;
+                break;
+        }
+        myBox.style.top = `${y}px`;
+        myBox.style.left = `${x}px`;
+    }
 });
 
-myButton.addEventListener("mouseover", event => {
-    console.log(event);
-    myBox.style.backgroundColor = "tomato";
-    myBox.textContent = "Hello!! 🤝"
-});
-
-myButton.addEventListener("mouseout", event => {
-    console.log(event);
-    myBox.style.backgroundColor = "tomato";
-    myBox.textContent = "Goodbye!! 🫡"
+document.addEventListener("keyup", event => {
+    myBox.textContent = "✅"
+    myBox.style.backgroundColor = "lightblue";
+    console.log(`Key up = ${event.key}`);
 });
 
