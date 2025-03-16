c6a50e47e662229a2d46c99d81f442fa5420df96
fenyediTheMerchant <fenyedi.levente@gmail.com>
2025-03-16 21:06:45 +0200

    65. Hide/Show HTML elements

--- index.js
+++ index.js
@@ -1,45 +1,17 @@
-// eventListener = listen for specific events to create interactive
-// web pages. Events: keydown, keyup
-// document.addEventListener(event, callback);
 
-const myBox = document.getElementById("myBox");
-const moveAmount = 10;
-let x = 0;
-let y = 0;
+const imgElement = document.getElementById("myImg");
+const myButton = document.getElementById("myButton");
 
+myButton.addEventListener("click", event => {
 
-document.addEventListener("keydown", event => {
-
-    if(event.key.startsWith("Arrow")){
-
-        event.preventDefault();
-
-        myBox.textContent = "‼️"
-        myBox.style.backgroundColor = "blue";
-        console.log(event.key);
-
-        switch(event.key){
-            case "ArrowUp":
-                y -= moveAmount;
-                break;
-            case "ArrowDown":
-                y += moveAmount;
-                break;
-            case "ArrowLeft":
-                x -= moveAmount;
-                break;
-            case "ArrowRight":
-                x += moveAmount;
-                break;
-        }
-        myBox.style.top = `${y}px`;
-        myBox.style.left = `${x}px`;
+    if(imgElement.style.visibility === "hidden"){
+        imgElement.style.visibility = "visible";
+        myButton.textContent = "Hide!";
+    }
+    else{
+        imgElement.style.visibility = "hidden";
+        myButton.textContent = "Show!";
     }
 });
 
-document.addEventListener("keyup", event => {
-    myBox.textContent = "✅"
-    myBox.style.backgroundColor = "lightblue";
-    console.log(`Key up = ${event.key}`);
-});
-
+console.log(imgElement);
