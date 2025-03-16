c294432d5b2b148b2beb927e94c45b8208cb963d
fenyediTheMerchant <fenyedi.levente@gmail.com>
2025-03-16 16:52:07 +0200

    56. Asynchronous code

--- index.js
+++ index.js
@@ -1,14 +1,45 @@
-// ES6 Module = A module is an externel file that contains
-// reusable code that can be imported into other JS files.
+// Synchronous = Executes line by line consecutively in a
+// sequantial manner. Code that waits for an operation to
+// complete.
 
-// Write reusable code for many different apps.
-// Can contain variables, classes, functions... and more
-// Introcued as part of ECMAScript 2015 update.
+// Asynchronous = Allows multiple operations to be performed
+// concurrently without waiting. Doesn't block the execution
+// flow and allows the program to continue
+// (I/O operations, network requests, fetching data)
+// Handled with: Callbacks, Promises, Async/Await
 
-import {PI, getCircumference, getArea, getVolume} 
-from './mathUtil.js';
+// Sync code
+// console.log("Task 1");
+// console.log("Task 2");
+// console.log("Task 3");
 
-console.log(PI);
-console.log(getCircumference(10));
-console.log(getArea(10));
-console.log(getVolume(10));
+// Async code
+// setTimeout is async function
+setTimeout(() => {
+    console.log("Task 1")
+    console.log("--------------------------------");
+    },2000);
+
+console.log("Task 2");
+console.log("Task 3");
+console.log("Task 4");
+
+// Async mode -> Ex. below = make sure task 1 runs before 2,3,4
+// Resolved with a callback
+function func1(callback){
+    setTimeout(() => {
+        console.log("Task 1");
+        // Use () here to call the function..
+        callback();
+    },3000);
+}
+
+function func2(){
+    console.log("Task 2");
+    console.log("Task 3");
+    console.log("Task 4");
+    console.log("--------------------------------");
+}
+
+// Dont use () in callback!!! IT will run immideatly
+func1(func2);
