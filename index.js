392c35b0330601366fb614d714da845ae7ac4cd7
fenyediTheMerchant <fenyedi.levente@gmail.com>
2025-03-16 14:47:53 +0200

    53. Digital Clock Program

--- index.js
+++ index.js
@@ -1,39 +1,16 @@
-// setTimeout() = a function in JavaScipr that allows you to
-// schedule the execution of a function after an amount of time (ms)
+// DIGI CLOCK
+// Using setInterval
 
-// Times are approximate (varies based on the workload of the
-// Javascript runtime env.) Shouldn't be used on stuff like stopwatch
-
-
-// setTimeout(callback, delay);
-// function sayHello(){
-//     window.alert("Hello");
-// }
-// setTimeout(sayHello, 3000);
-
-
-// setTimeout(anonymus func., delay);
-// setTimeout(function(){window.alert("Hello")}, 3000);
-
-
-// setTimeout(arronfunct., delay) 
-// setTimeout(() => window.alert("Hello"), 3000)
-
-
-// clearTimeout(timeoutId) = 
-// can cancel a timeout before it triggers
-
-// const timeoutId = setTimeout(() => window.alert("Hello"), 3000)
-// clearTimeout(timeoutId);
-
-let timeoutId;
-
-function startTimer(){
-    timeoutID = setTimeout(() => window.alert("Hello"), 1000); 
-    console.log("STARTED");
+function updateClock(){
+    
+    const now = new Date();
+    const hours = now.getHours().toString().padStart(2, 0);
+    const minutes = now.getMinutes().toString().padStart(2, 0);
+    const seconds = now.getSeconds().toString().padStart(2, 0);
+    const timeString = `${hours}:${minutes}:${seconds}`;
+    document.getElementById("clock").textContent = timeString;
 }
 
-function clearTimer(){
-    clearTimeout(timeoutID);
-    console.log("CLEARED");
-}
+updateClock();
+
+setInterval(updateClock, 1000);
