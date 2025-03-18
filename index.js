709c1c1c04a238ac495d330616d7b23f792769f5
fenyediTheMerchant <fenyedi.levente@gmail.com>
2025-03-18 16:44:06 +0200

    67. classList

--- index.js
+++ index.js
@@ -1,77 +1,63 @@
-// NodeList = Static collection of HTML elements by (id, class, element)
-// Can be created by using querySelectorAll()
-// Similar to an array, but no (map, filter, reduce)
+// classList = Element property in JavaScript used to interact
+// with an element's list of classes (CSS classes)
+// Allows you to make reusable classes for many elements across
+// your webpage.
 
-// NodeList won't update to automatically reflect changes
+// add()
+// remove()
+// toggle(remove if present, add if not)
+// replace(oldClass, newClass)
+// contains
+
+// const myH1 = document.getElementById("myH1");
+// const myButton = document.getElementById("myButton");
+
+// myH1.classList.add("enabled");
+// myButton.classList.add("enabled");
+
+// myButton.addEventListener("mouseover", event =>{
+//     event.target.classList.toggle("hover");
+// })
+// myButton.addEventListener("mouseout", event =>{
+//     event.target.classList.toggle("hover");
+// })
+// myButton.addEventListener("click", event =>{
+
+//     if(event.target.classList.contains("disabled")){
+//         event.target.textContent += "👍";
+//     }else{
+//         event.target.classList.replace("enabled","disabled");
+//     }
+
+// })
+// myH1.addEventListener("click", event =>{
+
+//     if(event.target.classList.contains("disabled")){
+//         event.target.textContent += "👍";
+//     }else{
+//         event.target.classList.replace("enabled","disabled");
+//     }
+
+// })
+
+// Challenge!
 
 let buttons = document.querySelectorAll(".myButtons");
-
-console.log(buttons);
-
-// ADD HTML/CSS PROPERTIES
-
-buttons.forEach(button =>{
-    button.style.backgroundColor = "green";
-    button.textContent += "👍";
-})
-
-// ADD CLICK EVENT LISTENER
-
 buttons.forEach(button => {
-    button.isRed = false;
-    button.addEventListener("click", event =>{
-        if(button.isRed){
-            event.target.style.backgroundColor = "green";
-            button.isRed = false;
+   button.classList.add("enabled"); 
+})
+buttons.forEach(button => {
+   button.addEventListener("mouseover", event =>{
+        event.target.classList.toggle("hover");
+   })
+   button.addEventListener("mouseout", event =>{
+        event.target.classList.toggle("hover");
+   })
+   button.addEventListener("click", event =>{
+       if(event.target.classList.contains("disabled")){
+           event.target.textContent = "DISABLED"
+        }else{
+            event.target.classList.replace("enabled","disabled");
         }
-        else{
-            event.target.style.backgroundColor = "red";
-            button.isRed = true;
-        }
-    });
-})
-
-// ADD MOUSEOVER AND OUT EVENT LISETENER
-buttons.forEach(button =>{
-    button.addEventListener("mouseover", event =>{
-        event.target.style.backgroundColor = "lightgreen";
-    })
-})
-
-buttons.forEach(button =>{
-    button.addEventListener("mouseout", event =>{
-        event.target.style.backgroundColor = "green";
-    })
-})
-
-// ADDING AN ELEMENT
-
-// Step.1 Creating the element
-const newButton = document.createElement("button"); 
-// Step.2 Add properties
-newButton.textContent = "Button 5";
-newButton.classList = "myButtons";
-// Step.3 Adding it to the DOM
-document.body.appendChild(newButton);
-
-console.log(buttons);
-
-// If we want to include the new button in the Nodelist we have to
-// redeclare the buttons as it doesnt reflect the changes from the automatically
-// DOM automaticaly
-
-buttons = document.querySelectorAll(".myButtons");
-
-console.log(buttons);
-
-// REMOVE / HIDE AN ELEMENT UPPON CLICK
-
-buttons.forEach(button =>{
-    button.addEventListener("click", event =>{
-        // event.target.style.visibility = "none"; 
-        event.target.remove();
-        // Here we update the nodelist based on the changed DOM
-        buttons = document.querySelectorAll(".myButtons");
-        console.log(buttons);
-    })
+   })
 })
