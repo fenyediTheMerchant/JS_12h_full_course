6c99eac1014296dab293d92aace26a54f3fb0277
fenyediTheMerchant <fenyedi.levente@gmail.com>
2025-03-18 16:14:08 +0200

    66. NodeLists

--- index.js
+++ index.js
@@ -1,17 +1,77 @@
+// NodeList = Static collection of HTML elements by (id, class, element)
+// Can be created by using querySelectorAll()
+// Similar to an array, but no (map, filter, reduce)
 
-const imgElement = document.getElementById("myImg");
-const myButton = document.getElementById("myButton");
+// NodeList won't update to automatically reflect changes
 
-myButton.addEventListener("click", event => {
+let buttons = document.querySelectorAll(".myButtons");
 
-    if(imgElement.style.visibility === "hidden"){
-        imgElement.style.visibility = "visible";
-        myButton.textContent = "Hide!";
-    }
-    else{
-        imgElement.style.visibility = "hidden";
-        myButton.textContent = "Show!";
-    }
-});
+console.log(buttons);
 
-console.log(imgElement);
+// ADD HTML/CSS PROPERTIES
+
+buttons.forEach(button =>{
+    button.style.backgroundColor = "green";
+    button.textContent += "👍";
+})
+
+// ADD CLICK EVENT LISTENER
+
+buttons.forEach(button => {
+    button.isRed = false;
+    button.addEventListener("click", event =>{
+        if(button.isRed){
+            event.target.style.backgroundColor = "green";
+            button.isRed = false;
+        }
+        else{
+            event.target.style.backgroundColor = "red";
+            button.isRed = true;
+        }
+    });
+})
+
+// ADD MOUSEOVER AND OUT EVENT LISETENER
+buttons.forEach(button =>{
+    button.addEventListener("mouseover", event =>{
+        event.target.style.backgroundColor = "lightgreen";
+    })
+})
+
+buttons.forEach(button =>{
+    button.addEventListener("mouseout", event =>{
+        event.target.style.backgroundColor = "green";
+    })
+})
+
+// ADDING AN ELEMENT
+
+// Step.1 Creating the element
+const newButton = document.createElement("button"); 
+// Step.2 Add properties
+newButton.textContent = "Button 5";
+newButton.classList = "myButtons";
+// Step.3 Adding it to the DOM
+document.body.appendChild(newButton);
+
+console.log(buttons);
+
+// If we want to include the new button in the Nodelist we have to
+// redeclare the buttons as it doesnt reflect the changes from the automatically
+// DOM automaticaly
+
+buttons = document.querySelectorAll(".myButtons");
+
+console.log(buttons);
+
+// REMOVE / HIDE AN ELEMENT UPPON CLICK
+
+buttons.forEach(button =>{
+    button.addEventListener("click", event =>{
+        // event.target.style.visibility = "none"; 
+        event.target.remove();
+        // Here we update the nodelist based on the changed DOM
+        buttons = document.querySelectorAll(".myButtons");
+        console.log(buttons);
+    })
+})
